92520d887a4afe89940686d1036abf10c83dcf71
Ibrahim Diallo <ibuvdiallo@gmail.com>
2025-05-31 19:49:22 -0700

    remove old version

--- public/scripts/utils/index.js
+++ public/scripts/utils/index.js
@@ -32,4 +32,11 @@ export const wait = (delay) => {
   });
 };
 
+export const timeCodeFormat = (seconds) => {
+  const h = Math.floor(seconds / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  const s = seconds % 60;
+  return [h, m, s].map((v) => (v < 10 ? "0" + v : v)).join(":");
+};
+
 export const clamp = (num, min, max) => Math.max(min, Math.min(num, max));
